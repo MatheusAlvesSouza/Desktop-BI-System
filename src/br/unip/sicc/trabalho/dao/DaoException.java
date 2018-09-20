e6fb9847559fdd28e2c7f5ad24f8b3a702c90e40
Matheus Alves <mat.alves.souza@outlook.com>
2018-09-20 00:04:51 -0300

    Exception

--- src/br/unip/sicc/trabalho/dao/DaoException.java
+++ src/br/unip/sicc/trabalho/dao/DaoException.java
@@ -1,5 +1,9 @@
 package br.unip.sicc.trabalho.dao;
 
-public class DaoException {
+public class DaoException extends Exception{
+	
+	public DaoException (String menssagem) {
+		super(menssagem);
+	}
 
 }
